ab33a038abc93b9777bc90da5423c41e5dfd2d0d
Dmytro <moiseenkodmitriy@i.ua>
2023-08-29 00:24:31 +0300

    deploy

--- src/js/fetchPixabay.js
+++ src/js/fetchPixabay.js
@@ -1,18 +1,18 @@
-import axios from "axios";
-import { pixabayKey, IMAGES_PER_PAGE } from "./utils/envConsts.js";
+import axios from 'axios';
+import { pixabayKey, IMAGES_PER_PAGE } from './utils/envConsts.js';
 
 const axiosPixabay = axios.create({
-  baseURL: "https://pixabay.com/api/",
+  baseURL: 'https://pixabay.com/api/',
   params: {
-    key: pixabayKey,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: "true",
+    key: '31420131-aff65dfb3f4bd8a8d020782c7',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
     per_page: IMAGES_PER_PAGE,
   },
 });
 
 export async function fetchPixabayImages(q, page) {
-  const { data } = await axiosPixabay.get("", { params: { q, page } });
+  const { data } = await axiosPixabay.get('', { params: { q, page } });
   return data;
 }
