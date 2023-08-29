7664a50dbda28d707bb1e58aa900e1320a4e3e67
Dmytro <moiseenkodmitriy@i.ua>
2023-08-29 11:19:01 +0300

    pixabay

---
+++ src/js/utils/envConsts.js
@@ -0,0 +1,7 @@
+export const pixabayKey = '31420131-aff65dfb3f4bd8a8d020782c7';
+export const IMAGES_PER_PAGE = 40;
+export const LIGHTBOX_PARAMS = {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+};
